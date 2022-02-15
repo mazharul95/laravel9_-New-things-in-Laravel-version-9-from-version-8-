7072c2a922325eea3c4bb48376d215ded5321c0d
mazharul95 <mipiyas07@gmail.com>
2022-02-15 23:51:23 +0600

    Controller Route Groups

---
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(191);
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email', 250)->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+
+            $table->unique([DB::raw('email(191)')]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password_resets');
+    }
+};
